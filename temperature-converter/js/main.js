e13794f76c9cae82b449894bdb6f7d90ecaa5a21
nbarnabee <nicole.barnabee@gmail.com>
2022-02-27 20:42:51 +0100

    Commit solution

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,1 +1,12 @@
 //Write your pseduo code first! 
+//1. get variable
+//2. convert variable
+//3. return variable
+
+document.querySelector("button").addEventListener("click", tempConvert);
+
+function tempConvert() {
+  tempCel = Number(document.querySelector("#input").value);
+  tempFah = tempCel * 1.8 + 32;
+  document.querySelector(".return").innerText = tempFah;
+}
