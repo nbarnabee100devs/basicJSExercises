bb6a5e88b0a65e150ba4f50cd6d3aebf04ad9787
nbarnabee <nicole.barnabee@gmail.com>
2022-02-28 12:49:54 +0100

    Rewrite solutions with arrow functions

--- simple-functions/js/main.js
+++ simple-functions/js/main.js
@@ -1,15 +1,15 @@
 //create a function that adds two numbers and alerts the sum
-function makeSum(x, y) {
-  alert(x + y);
-}
+let sum = (x,y) => alert(x+y);
 
 //create a function that multiplys three numbers and console logs the product
-function multiThree(x, y, z) {
-  console.log(x*y*z);
-}
+let multi = (x,y,z) => console.log(x*y*z);
 
 //create a function that divides two numbers and returns the ???
-function divTwo(x, y) {
-  z = x/y;
-  return z;
-}
+
+let div = (x, y) => x/y;
+
+let ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+ask ("Do you agree?",
+() => alert("You agreed."),
+() => alert("You cancelled the execution."));
