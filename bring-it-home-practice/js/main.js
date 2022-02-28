03930f4677ea5d33555bccaee9d4df6830c1d9d0
nbarnabee <nicole.barnabee@gmail.com>
2022-02-28 12:59:29 +0100

    Rewrite the solutions with arrow functions

--- bring-it-home-practice/js/main.js
+++ bring-it-home-practice/js/main.js
@@ -1,39 +1,32 @@
 // *Variables*
 // Create a variable and console log the value
+
 let foo = 10;
 console.log(foo);
+
+
 // Create a variable, add 10 to it, and alert the value
-let bar = 5;
-bar += 5;
-alert(bar);
+
+let bar = 15;
+alert(bar += 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
-function subFour(x, y, z, b) {
-  alert(x-y-z-b);
-}
+let subFour = (x, y, z, a) => alert(x-y-z-a);
+
+
 
 // Create a function that divides one number by another and returns the remainder
 
-function remainder(x, y) {
-  let z = x%y;
-  return z;
-}
+let remainder = (x, y) => x%y;
+
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
-function jumCheck(x,y) {
-  z = x + y;
-  if (z > 50) alert("Jumanji");
-  else return;
-}
+let jumanji = (x, y) => ((x+y)>50) ? alert("Jumanji") : (x+y);
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
-function zebraCheck(x, y, z){
-  let zCheck = x*y*z;
-  if ((zCheck % 3) == 0) alert("ZEBRA");
-  else return;
-}
+let zebra = (x, y, z) => ((x*y*z)%3===0) ? alert("Zebra") : x;
