b90699a8ba39dcdd1cc76c70260ad0f023898693
nbarnabee <nicole.barnabee@gmail.com>
2022-03-02 07:44:19 +0100

    Initial commit

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -1,17 +1,6 @@
-//Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
+document.getElementById("check").addEventListener("click", checkDate);
 
-document.querySelector('#check').addEventListener('click', check)
-
-function check() {
-
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
-  }
-
+function checkDate() {
+  let dateValue = document.getElementById("day").value.toLowerCase();
+  console.log(dateValue);
 }
