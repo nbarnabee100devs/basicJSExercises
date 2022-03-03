813c7163c4410bc9d9043a7d2fda63c1e45a6228
nbarnabee <nicole.barnabee@gmail.com>
2022-03-03 20:51:43 +0100

    Rewrite solutions

--- bring-it-home-practice/js/main.js
+++ bring-it-home-practice/js/main.js
@@ -13,20 +13,21 @@ alert(bar += 10);
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
-let subFour = (x, y, z, a) => alert(x-y-z-a);
 
+let subFour = (x, y, z, a) => alert(x-y-z-a);
 
 
 // Create a function that divides one number by another and returns the remainder
 
-let remainder = (x, y) => x%y;
+let division = (x, y) => x%y;
+
 
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
-let jumanji = (x, y) => ((x+y)>50) ? alert("Jumanji") : (x+y);
+let jumanji = (x, y) => x + y > 50 ? alert("Jumanji") : console.log("nope");
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
-let zebra = (x, y, z) => ((x*y*z)%3===0) ? alert("Zebra") : x;
+let zebra = (x, y, z) => ((x*y*z)%3 === 0) ? alert("Zebra") : console.log("nope");
