6a986583a40a1b682bd27f78470ec717d886fc4f
nbarnabee <nicole.barnabee@gmail.com>
2022-03-03 21:04:47 +0100

    Rewrite solution

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -1,6 +1,17 @@
-document.getElementById("check").addEventListener("click", checkDate);
+document.getElementById("check").onclick = dayCheck;
 
-function checkDate() {
-  let dateValue = document.getElementById("day").value.toLowerCase();
-  console.log(dateValue);
+function dayCheck() {
+  const day = document.getElementById("day").value.toLowerCase();
+  const response = document.getElementById("placeToSee");
+  if (day === "sunday" || day === "saturday") 
+    response.innerHTML = "It's the weekend!";
+ else if (day === "monday" || day === "wednesday")
+    response.innerHTML = "Booooring!";
+ else if (day === "tuesday" || day === "thursday")
+    response.innerHTML = "Class day!!";
+ else if (day === "friday")
+    response.innerHTML = "Thank god!!";
+  else if  (day.includes("day"))
+    response.innerHTML = "What kind of a day is that??";
+  else response.innerHTML = "That's not a day!!";
 }
