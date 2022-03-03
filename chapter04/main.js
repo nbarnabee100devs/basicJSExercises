d0410da6c88756f262f4218daac526e65f1ff086
nbarnabee <nicole.barnabee@gmail.com>
2022-03-03 22:47:45 +0100

    Rewrite solutions

--- chapter04/main.js
+++ chapter04/main.js
@@ -1,68 +1,56 @@
-let turn = 1;
-while (turn <= 10) {
-  console.log(`Turn number ${turn}!`);
-  turn++;
+function carousel(num) {
+  for (i = 1; i <= num; i++)
+    console.log(`Turn number ${i}`)
 }
 
-for (i=1; i<=10; i++) 
-  console.log(`Turn number ${i}!`);
-
-function carousel(x) {
-  for (i = 1; i <= x; i++)
-    console.log(`Turn number ${i}!`);
-}
-
-function parity(x) {
-  for (let i = 1; i <= 10; i++) {
-    if (x % 2 === 0) {
-      console.log(`${x} is even`);
-    }
-    else console.log(`${x} is odd`);
-    x++;
+function parity(num) {
+  for (i = 1; i <= 10; i++) {
+    (num % 2 === 0) ?
+    console.log(`${num} is even`) :
+    console.log(`${num} is odd`);
+    num++;
   }
 }
 
-function inputVal() {
-  x = prompt("Please enter a number between 50 and 100");
-  while (x <= 50 || x >= 100) x = prompt("Please enter a number between 50 and 100");
+function numPrompt() {
+let num = prompt("Please enter a number between 50 and 100");
+while (num <= 50 || num >= 100) 
+num = prompt("Please enter a number between 50 and 100");
 }
 
-function multiTable() {
-  x = prompt("Please enter a number between 2 and 9, inclusive");
-  while (x <= 1 || x >= 10) x = prompt("Please enter a number between 2 and 9, inclusive");
-  for (i = 1; i<=12; i++) 
-  console.log(x*i);
+function multiTable(num) {
+  for (i = 1; i <= 12; i++)
+  console.log(i*num);
+}
+
+function multiTableBound() {
+  let num = prompt("Please enter a number between 2 and 9");
+  while (num >= 9 || num <=2)
+  num = prompt("Please enter a number between 2 and 9");
+  for (i = 1; i <= 12; i++)
+  console.log(i*num);
 }
 
 function yesOrNo() {
-  answer = prompt("Password, please");
-  while (answer.toLowerCase() !=="yes" && answer.toLowerCase() !=="no") 
-  answer = prompt("Try again.");
-  alert ("Correct!");
+  let response = prompt("Please enter a word");
+  while (response.toLowerCase() !== "yes" && response.toLowerCase() !== "no")
+  response = prompt("Please enter a word");
 }
 
 function fizzBuzz() {
-  for (i = 1; i <= 100; i++) {
-    if (i%3 === 0 && i%5 === 0) console.log("FizzBuzz");
-    else if (i%3 === 0) console.log("Fizz");
-    else if (i%5 === 0) console.log("Buzz");
-    else console.log(i);
-  }
-}
-
-function fizzBuzzSwitch() {
-  for (i = 1; i <= 100; i++) {
-    switch(true) {
-      case ((i%3 === 0) && (i%5 === 0)): 
-        console.log("FizzBuzz");
+  for (i=1; i <= 100; i++) {
+  switch (true) {
+    case (i%3 == 0 && i%5 == 0):
+      console.log("FizzBuzz");
       break;
-      case (i%3 === 0):
-        console.log("Fizz");
-        break;
-      case (i%5 === 0):
-        console.log("Buzz");
-        break;
-      default: console.log(i);
-    }
+    case (i%3 == 0):
+      console.log("Fizz");
+      break;
+    case (i%5 == 0):
+      console.log("Buzz");
+      break;
+    default:
+      console.log(i);
+  }
   }
 }
