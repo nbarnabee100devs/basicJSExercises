5e80256af52b04ea16df5359cc72c92045a25263
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-03-22 12:17:09 +0100

    Add more solutions

--- jsInfo/data_types/arrays/array_methods/main.js
+++ jsInfo/data_types/arrays/array_methods/main.js
@@ -65,3 +65,88 @@ function copySorted(arr) {
 
 
   /* Calculator */
+
+  // skip this one
+
+
+  /* Map to Names
+You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+ */
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(e => e.name);
+
+console.log(names); // John, Pete, Mary
+
+
+/* Sort by age
+
+Write the function sortByAge(users) that gets an array of objects with the age property and sorts them by age.
+*/
+
+let sortByAge = function(users) {return users.sort((a, b) => a.age < b.age ? -1 : 1)};
+
+function sortByAge2(users) {
+  return users.sort((a, b) => a.age < b.age ? -1 : 1);
+};
+
+/*  Shuffle an array
+Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+*/
+
+// Could make a 2nd array with a length = arr.length
+// 2nd array would contain randomly ordered numbers from 0 - array.length -1
+
+function shuffle(arr) {
+
+}
+
+/* Get average age 
+Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age. */
+
+function getAverageAge(users) {
+  let average = 0;
+  users.forEach(a => average += a.age);
+  return average/users.length;
+}
+
+/* the "official" solution */
+
+function getAverageAge(users) {
+  return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+}
+
+
+/* Filter unique array members
+Let arr be an array.
+
+Create a function unique(arr) that should return an array with unique items of arr.
+*/
+
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+function unique(arr) {
+  let uniqueArr = [];
+  
+  for (let string of arr) {
+    if (!uniqueArr.includes(string))
+    uniqueArr.push(string);
+  };
+    console.log(uniqueArr);
+  };
+
+
+  /* Create keyed object from array
+
+  Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+  */
