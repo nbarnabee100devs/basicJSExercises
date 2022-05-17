59ae74228559413680e424d065da6ca80197dbde
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-05-17 09:07:49 +0200

    Add code

--- code_along_practice/main.js
+++ code_along_practice/main.js
@@ -1,1 +1,28 @@
 // Async/Await
+
+console.log("sync 1");
+
+setTimeout(_ => console.log("Timeout 2"), 0);
+
+Promise.resolve().then(_ => console.log("promise"));
+
+console.log("sync 2");
+
+// synch 1 will be executed first
+// Timeout and promise queued, but the promise will fire first because microtasks take precedence
+// sync 2 is executed on the second loop, promise follows after, and then timeout
+
+const promise = fetch(`https://www.dnd5eapi.co/api/spells/acid-arrow`)
+
+promise
+  .then(res => res.json())
+  .then(data => console.log(data.name));
+
+
+console.log("sync 3");
+
+// now this is interesting: when I run it, all three of the synchs appear, then the first promise, then the timeout, and then the second promise
+
+setTimeout(_ => console.log("TImeout 3"), 0);
+
+// timeout 3 also appears before the promise is resolved; I guess it's just taking a long time to return the data
